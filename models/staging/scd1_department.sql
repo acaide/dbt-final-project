7cc1e0b0160af559c1b7daa30e312f2486a0b771
Andrew Caide <andrewcaide@gmail.com>
2024-11-13 21:55:51 +0000

    scd1 steps work!

--- models/staging/scd1_department.sql
+++ models/staging/scd1_department.sql
@@ -10,9 +10,9 @@ WITH updated_data AS (
     SELECT * 
     FROM {{ ref('stg_wallmart__department') }} s
     LEFT JOIN {{ source('SOURCE','DEPARTMENT') }} target
-        ON target.store_id = s.store_id
+        ON target.location_id = s.location_id
     WHERE
-        target.store_id IS NULL  -- condition for new store
+        target.location_id IS NULL  -- condition for new store
         OR target.dept_id != s.dept_id -- conditions for updated values
         OR target.date != s.date
         OR target.weekly_sales != s.weekly_sales
---
+++ models/staging/scd1_store.sql
@@ -0,0 +1,19 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key='store_id') 
+}}
+
+-- apply SCD1 logic
+
+WITH updated_data AS (
+    SELECT * 
+    FROM {{ ref('stg_wallmart__store') }} s
+    LEFT JOIN {{ source('SOURCE','STORE') }} target
+        ON target.location_id = s.location_id
+    WHERE
+        target.location_id IS NULL  -- condition for new store
+        OR target.size != s.size
+)
+
+SELECT * FROM updated_data
