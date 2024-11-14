f1873aa67de12edace4a49ac6be6f87bba777282
Andrew Caide <andrewcaide@gmail.com>
2024-11-14 22:58:25 +0000

    SCD1 tables appear functional

--- models/staging/dim_wallmart__store.sql
+++ models/staging/dim_wallmart__store.sql
@@ -25,17 +25,16 @@ changes AS (
         , CASE 
             WHEN e.store_id IS NULL OR e.dept_id IS NULL THEN CURRENT_TIMESTAMP 
             ELSE e.insert_date  -- Keep existing insert_date if record already exists
-        END AS insert_date,
+        END AS insert_date
         -- Update update_date when store_type or store_size changes
         , CASE 
-            WHEN 
-                e.store_id IS NULL 
-                OR e.store_type != s.store_type 
+            WHEN e.store_id IS NULL OR e.dept_id IS NULL THEN NULL
+            WHEN e.store_type != s.store_type 
                 OR e.store_size != s.store_size THEN CURRENT_TIMESTAMP
-            ELSE NULL
+            ELSE e.update_date
         END AS update_date
-    FROM source_data s
-    LEFT JOIN existing_data e
+    FROM new_data s
+    LEFT JOIN {{ source('EXISTING','STORE') }} e
         ON s.store_id = e.store_id
         AND s.dept_id = e.dept_id
 )
